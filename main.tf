e0114a76044d0c583f4e35e5e15ec3347c3149b6
shujath-git900 <shujath.devops@gmail.com>
2026-01-31 22:07:34 +0530

    missing variable in module fix

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ module "ec2" {
   source = "./module"
   for_each = var.tool_name
 
-  tool_name = each.key
-  instance_type = each.value["instance_type"]
+  tool_name             = each.key
+  instance_type         = each.value["instance_type"]
+  policy_resource_list  = each.value["policy_resource_list"]
   zone_id       = var.zone_id
 }
