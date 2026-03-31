3d0a2f2cc3defdf8fc4770b774545efcbf59e30b
shujath-git900 <shujath.devops@gmail.com>
2026-03-31 11:11:06 +0530

    spot instance

--- module/main.tf
+++ module/main.tf
@@ -6,11 +6,21 @@ terraform {
     }
   }
 }
+
 resource "aws_instance" "instance" {
   ami                     = data.aws_ami.ami.id
   instance_type           = var.instance_type
   vpc_security_group_ids  = [data.aws_security_group.selected.id]
   iam_instance_profile    = aws_iam_instance_profile.profile.name
+
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type             = "persistent"
+    }
+  }
+
   tags = {
       Name = var.tool_name
   }
